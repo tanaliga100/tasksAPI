e7a6559b13b356781b50798c7be3b56e2573aae5
tanaliga100 <43674051+tanaliga100@users.noreply.github.com>
2024-05-15 14:52:08 +0800

    delete-dist-folder

--- dist/middlewares/notFound.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var notFound = function (req, res) {
-    res.status(404).send("<small style=\"textAlign=\"center\"\">I cannot find what you are looking for</small>\n  <a href=\"/\">Go Back</a>\n  ");
-};
-exports.default = notFound;
--- dist/middlewares/notFoundMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundMiddleware = void 0;
-var notFoundMiddleware = function (req, res, next) {
-    res.status(404).send("<small style=\"textAlign=\"center\"\">Route does not exist</small>\n  <a href=\"/\">Go Back</a>\n  ");
-};
-exports.notFoundMiddleware = notFoundMiddleware;
